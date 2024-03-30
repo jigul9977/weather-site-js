c35dcbb46214ab0067fb7ba608269b71873214d8
jigul9977 <arashtrader9977@gmail.com>
2024-03-30 15:55:10 +0330

    complete the section of current weather by coordinate

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,24 @@
-const BASE_URL =
-  //weather?q=Tehran&appid=48336baaf9501606abfd8dbec72a9ce8
-  "https://api.openweathermap.org/data/2.5";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "48336baaf9501606abfd8dbec72a9ce8";
 
 const searchBox = document.getElementById("search-box");
 const searchButton = document.getElementById("search-btn");
 const todayWeatherSection = document.getElementById("today-weather-sec");
+const locationIcon = document.getElementById("location-icon");
 
-const getcurrentweather = async (cityName) => {
+const getcurrentweatherByCityName = async (cityName) => {
   const url = `${BASE_URL}/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
   const response = await fetch(url);
   const json = await response.json();
   return json;
 };
+const getcurrentweatherByCoordinate = async (lat, lng) => {
+  const url = `${BASE_URL}/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
+
 const showCurrentWeather = (data) => {
   const curentWeatherJSX = `
   <h1>${data.name}, ${data.sys.country} </h1>
@@ -27,6 +33,21 @@ const showCurrentWeather = (data) => {
   </div>`;
   todayWeatherSection.innerHTML = curentWeatherJSX;
 };
+const PositionCallback = async (position) => {
+  let { latitude, longitude } = position.coords;
+  const currentWeather = await getcurrentweatherByCoordinate(latitude, longitude);
+  showCurrentWeather(currentWeather);
+};
+const PositionErrorCallback = (error) => {
+  console.log(error);
+};
+const locationHandler = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(PositionCallback, PositionErrorCallback);
+  } else {
+    alert("navigator.geolocation is not available");
+  }
+};
 
 const searchHandler = async () => {
   const cityName = searchBox.value;
@@ -38,3 +59,4 @@ const searchHandler = async () => {
 };
 
 searchButton.addEventListener("click", searchHandler);
+locationIcon.addEventListener("click", locationHandler);
